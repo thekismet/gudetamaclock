548bca2f79aebe05b9f97774a8dd32e7692c1d0f
thekismet <38202378+thekismet@users.noreply.github.com>
2020-10-21 18:04:26 +0900

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -10,47 +10,47 @@ weatherid = wid()
 def graphics_path():
 
 	if hour == 1:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleepingbaby_white_back.png"
 	elif hour == 2:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleepingbaby_white_back.png"
 	elif hour == 3:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleepingbaby_white_back.png"
 	elif hour >= 4 and hour <= 6:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleepingbaby_white_back.png"
 	elif hour == 7:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleepingbaby_white_back.png"
 	elif hour == 8:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/tumble.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/goodmorning_white_back.png"
 	elif hour == 9:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/tired.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/mohana_white_back.png"
 	elif hour == 10:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/what.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/elsa2_white_back.png"
 	elif hour == 11:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/laidback.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/rapunzel7_white_back.png"
 	elif hour == 12:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/reverse.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/mary_white_back.png"
 	elif hour == 13:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/shell.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/rapunzel_white_back.png"
 	elif hour == 14:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/flower.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/rapunzel5_white_back.png"
 	elif hour == 15:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/chill.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/elsa5_white_back.png"
 	elif hour == 16:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/mulan_white_back.png"
 	elif hour == 17:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/dozing.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/bell3_white_back.png"
 	elif hour == 18:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/meh.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/ana2_white_back.png"
 	elif hour == 19:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/whatever.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/rapunzel4_white_back.png"
 	elif hour == 20:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/wa.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/beauty_beast_red_back.png"
 	elif hour == 21:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/bed.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/bed_white_back.png"
 	elif hour == 22:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleeping_princess_white_back.png"
 	elif hour == 23:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"	
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleeping_princess_white_back.png"	
 
 	else:
 		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
@@ -96,8 +96,8 @@ def graphics_path():
 	elif day == '1031' and hour > 20:
 		graphicspath = "/home/pi/clock/gudetamaclock/graphics/jackolantern.png"
 	elif day == '1225':
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/christmas.png"
-	elif day == '1231':
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/xmas_white_back.png"
+	elif day == '0912':
 		graphicspath = "/home/pi/clock/gudetamaclock/graphics/birthday.png"
 	else:
 		graphicspath = graphicspath
