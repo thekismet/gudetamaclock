100a653fc18fc9146e024f309b5f315c4c5bf482
thekismet <38202378+thekismet@users.noreply.github.com>
2020-10-21 18:52:35 +0900

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -53,52 +53,54 @@ def graphics_path():
 		graphicspath = "/home/pi/clock/gudetamaclock/graphics/sleeping_princess_white_back.png"	
 
 	else:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/zzz.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/whitesnow_white_back.png"
 
 # Weekday
 
 	if weekday == 5 and hour > 14 and hour <= 17:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/friday.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/super_white_back.png"
 	else:
 		graphicspath = graphicspath
 	
 # Weathers	
-
+	# extream rain
 	if weatherid >= 502 and weatherid <= 504:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/brolly.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/mary_white_back.png"
+	# heavy snow
 	elif weatherid == 602 or weatherid == 622:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/snow.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/snow_white_back.png"
+	# cloud
 	elif weatherid == 804:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/meringue.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/aurora.png"
 	else:
 		graphicspath = graphicspath
 	
 # Special days
 
 	if day == '0101':
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/ballons.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/rapunzel6_white_back.png"
 	elif day == '0214':
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/valentine.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/whitesnow_white_back.png"
 	elif day == '0405' and year == 2021:
-		graphicspath = "/home/pi/clock/gudetamaclockgraphics/bunny.png"
+		graphicspath = "/home/pi/clock/gudetamaclockgraphics/tinkerbell_white_back.png"
 	elif day == '0418' and year == 2022:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/bunny.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/tinkerbell_white_back.png"
 	elif day == '0410' and year == 2023:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/bunny.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/tinkerbell_white_back.png"
 	elif day == '1031' and hour > 12 and hour <= 14:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/xray.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/snowman_white_back.png"
 	elif day == '1031' and hour > 14 and hour <= 16:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/boo.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/snowman_white_back.png"
 	elif day == '1031' and hour > 16 and hour <= 18:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/halloween.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/cinderella_full_white_back.png"
 	elif day == '1031' and hour > 18 and hour <= 20:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/fairy.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/cinderella_full_white_back.png"
 	elif day == '1031' and hour > 20:
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/jackolantern.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/jcinderella_full_white_back.png"
 	elif day == '1225':
 		graphicspath = "/home/pi/clock/gudetamaclock/graphics/xmas_white_back.png"
 	elif day == '0912':
-		graphicspath = "/home/pi/clock/gudetamaclock/graphics/birthday.png"
+		graphicspath = "/home/pi/clock/gudetamaclock/graphics/pumpkin_white_back.png"
 	else:
 		graphicspath = graphicspath
 	
